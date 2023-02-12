779411c73e8e749a1dc408036a9acbc360cabc3a
Xsannn <xsannn@gmail.com>
2023-02-12 19:15:28 +0300

    Mock data and state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Player from './components/Player';
 import Song from './components/Song';
+import chillHop from './data';
 
 import './styles/app.scss';
 
 const App = () => {
+	const [songs, setSongs] = useState(chillHop());
+	const [currentSong, setCurrentSong] = useState(songs[0]);
+
 	return (
 		<div>
-			<Song />
+			<Song currentSong={currentSong} />
 			<Player />
 		</div>
 	)
